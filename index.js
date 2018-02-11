73235783bc516aea0db3f6119be460a080562f82
Dmitriy Beglov <ditransler@gmail.com>
2018-02-11 14:14:03 +0400

    Tried out Markdown for answers

--- index.js
+++ index.js
@@ -25,15 +25,12 @@ const bot = new TelegramBot(TOKEN, {
 });
 
 bot.on('message', msg => {
-    const html = `
-        <strong>Hello, ${msg.from.first_name}</strong>
-        <i>Test Message</i>
-        <pre>
-            ${debug(msg)}
-        </pre>
+    const markdown = `
+        *Hello, ${msg.from.first_name}*
+        _italic text_
     `;
 
-    bot.sendMessage(msg.chat.id, html, {
-        parse_mode: 'HTML'
+    bot.sendMessage(msg.chat.id, markdown, {
+        parse_mode: 'Markdown'
     });
 });
