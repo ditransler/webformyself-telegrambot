762fc7db843854aece2fccc3ba307ad58db09747
Dmitriy Beglov <ditransler@gmail.com>
2018-02-19 22:03:41 +0400

    Added mongoose connect

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 process.env["NTBA_FIX_319"] = 1; // fix for the node-telegram-bot-api module's issue
 
 const TelegramBot = require('node-telegram-bot-api');
+const mongoose = require('mongoose');
 const helpers = require('./helpers');
 const config = require('../config');
 const keyboard = require('./keyboard');
 const keyboardButtons = require('./keyboard-buttons');
 
 helpers.logStart();
+
+mongoose.connect(config.DB_URL)
+.then(() => console.log('MongoDB connected.'))
+.catch((err) => console.log('MongoDB connection failed', err));
 
 const bot = new TelegramBot(config.TOKEN, {
     polling: true
